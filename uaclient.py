13aec1eab62c9e0d6a548828ef97ebc979f7bf4d
Yolanda Lillo Mata <ylillo@gamma19.aulas.gsyc.urjc.es>
2018-12-05 10:08:08 +0100

    2.3.1 metodos

--- uaclient.py
+++ uaclient.py
@@ -5,14 +5,24 @@ import sys
 import time
 
 try:
-    CONFIG = sys.argv[1]
-    METODO = sys.argv[2]
-    OPCION = sys.argv[3]
+    CONFIG = sys.argv[1] #Fichero XML
+    METODO = sys.argv[2] #Metodo SIP
+    OPCION = sys.argv[3] #Parametro opcional
 except (IndexError, ValueError):
     sys.exit("Usage: python3 uaclient.py config method option")
 
 
+class EchoHandler(socketserver.DatagramRequestHandler):
+    """Echo server class."""
+    def handle(self):
+        while 1:
+            if METODO == 'REGISTER':
 
+            if METODO == 'INVITE':
+
+            if METODO == 'ACK':
+
+            if METODO == 'BYE'
 
 
 if __name__ == "__main__":
