b1646594811b7bcc92cfdf2a1a6be03e63e0bf4b
Yolanda Lillo Mata <ylillo@alpha.aulas.gsyc.urjc.es>
2018-12-13 17:18:13 +0100

    Ver diccionario con regproxy que me faltaba

--- uaclient.py
+++ uaclient.py
@@ -5,6 +5,7 @@ import sys
 import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import time
 
 class ficheroXML(ContentHandler):
     def __init__(self):
@@ -13,9 +14,10 @@ class ficheroXML(ContentHandler):
             "uaserver": ["ip", "puerto"],
             "rtpaudio": ["puerto"],
             "log": ["path"],
+	    "regproxy": ["ip","puerto"],
             "audio": ["path"],
         }
-        self.datos = []
+        self.lista = []
         self.etiquetas = self.diccionario.keys() #Para obtener las claves del diccionario.
 
     def startElement(self, name, attrs):
@@ -24,12 +26,15 @@ class ficheroXML(ContentHandler):
             dic_etiqueta['etiqueta'] = name
             for atributo in self.diccionario[name]:
                 dic_etiqueta[atributo] = attrs.get(atributo, "")
-            self.datos.append(dic_etiqueta)
+            self.lista.append(dic_etiqueta)
 
     def get_tags(self):
-        return self.datos
+        return self.lista
 
-
+def log(message):
+    #Fichero log
+    fich.write(time.strftime('%Y%m%d%H%M%S '))
+    fich.write(message +"\r\n")
 
 
 if __name__ == "__main__":
@@ -40,11 +45,18 @@ if __name__ == "__main__":
         CONFIG = sys.argv[1] #FicheroXML
         METODO = sys.argv[2]  # Metodo SIP
         OPCION = sys.argv[3]  # Parametro opcional
-        parser = make_parser()
-        cHandler = ficheroXML()
-        parser.setContentHandler(cHandler)
-        parser.parse(open(sys.argv[1]))
-        datos = cHandler.get_tags()
-        print(datos)
+
     except (IndexError, ValueError):
         sys.exit("Usage: python3 uaclient.py config method option")
+
+    parser = make_parser()
+    cHandler = ficheroXML()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(CONFIG))
+    lista = cHandler.get_tags()
+    for dic in lista:
+        print(dic)
+
+
+
+
