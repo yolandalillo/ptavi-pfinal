d5bea8f28e8b8e21ea1f13c823a5c19c761516c8
Yolanda Lillo Mata <ylillo@alpha.aulas.gsyc.urjc.es>
2018-12-15 20:27:25 +0100

    Definimos actual time, falta log y metodo ack

--- uaclient.py
+++ uaclient.py
@@ -1,66 +1,94 @@
 #!/usr/bin/python3
- # -*- coding: utf-8 -*-
- 
+# -*- coding: utf-8 -*-
+
 import sys
 import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
- 
+import time
+
+
 class ficheroXML(ContentHandler):
     def __init__(self):
         self.diccionario = {
             "account": ["username", "passwd"],
             "uaserver": ["ip", "puerto"],
             "rtpaudio": ["puerto"],
-	    "regproxy":["ip","puerto"],
+            "regproxy": ["ip", "puerto"],
             "log": ["path"],
             "audio": ["path"],
         }
         self.config = {}
- 
+
     def startElement(self, name, attrs):
         if name in self.diccionario:
             for atribute in self.diccionario[name]:
                 self.config[name+"_"+atribute] = attrs.get(atribute, "")
- 
+
     def get_tags(self):
         return self.config
 
-def log(message):
-     #Fichero log
-     fich.write(time.strftime('%Y%m%d%H%M%S '))
-     fich.write(message +"\r\n")
+def actually_time():
+	time_actual = time.time()
+	return time.strftime("%Y%m%d%H%M%S ",time.gmtime(time.time()))
+
+
+"""
+def log():
+"""
 
 if __name__ == "__main__":
-     """
-     Programa principal
-     """
-     try:
-        CONFIG = sys.argv[1] #FicheroXML
-        METODO = sys.argv[2]  # Metodo SIP
-        OPCION = sys.argv[3]  # Parametro opcional
-     except (IndexError, ValueError):
+    """
+    Programa principal
+    """
+    try:
+        CONFIG = sys.argv[1]  # FicheroXML.
+        METODO = sys.argv[2]  # Metodo SIP.
+        OPCION = sys.argv[3]  # Parametro opcional.
+    except (IndexError, ValueError):
         sys.exit("Usage: python3 uaclient.py config method option")
 
-	#Parse el fichero XML
-     parser = make_parser()
-     cHandler = ficheroXML()
-     parser.setContentHandler(cHandler)
-     parser.parse(open(CONFIG))
+# Parse el fichero XML.
+    parser = make_parser()
+    cHandler = ficheroXML()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(CONFIG))
+    lista = cHandler.get_tags()
+    print(lista)
+# Variables necesarias.
+    USERNAME = lista['account_username']
+    USERPASSW = lista['account_passwd']
+    SERVER = lista['uaserver_ip']
+    PORT = lista['uaserver_puerto']
+    AUDIOPORT = lista['rtpaudio_puerto']
+    FILEAUDIO = lista['audio_path']
+    FILELOG = lista['log_path']
+    IPPROXY = lista['regproxy_ip']
+    PUERTOPROXY = lista['regproxy_puerto']
+    PORTRTP = lista['rtpaudio_puerto']
 
-     lista = cHandler.get_tags()
-     print(lista)
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IPPROXY, int(PUERTOPROXY)))
 
-     USERNAME = lista['account_username']
-     SERVER = lista['uaserver_ip']
-     PORT = lista['uaserver_puerto']
-    
+        if METODO == 'BYE':
+            LINE = METODO + " sip:" + OPCION + " SIP/2.0\r\n"
+        elif METODO == 'REGISTER':
+            LINE = METODO + ' sip:' + USERNAME + ':' + PORT + ' SIP/2.0\r\n'
+            LINE += "Expires: " + OPCION + "\r\n"
+            print(LINE)    
+        elif METODO == 'INVITE':
+            LINE = "INVITE " + "sip:" + OPCION + " SIP/2.0\r\n"
+            LINE += "Content-Type: application/sdp\r\n\r\n"
+            LINE += "v=0\r\n" + "o=" + USERNAME + " " + SERVER + " \r\n"
+            LINE += "s=misesion" + "\r\n" + "t=0" + "\r\n"
+            LINE += "m=audio " + PORTRTP + " RTP" + "\r\n"
+            print(LINE)
+        elif METODO == 'ACK':
+            print('yolanda')
 
-	
+        print("Terminando socket...")
 
-
-
-
-    
-
-
+except ConnectionRefusedError:
+    print("20101018160243 Error: No server listening at" + SERVER + 'port' + PORT)
