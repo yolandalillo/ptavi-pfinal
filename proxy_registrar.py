7f96b3828dce1dec1fdd874d1b0dbaeb183829a4
Yolanda Lillo Mata <ylillo@alpha.aulas.gsyc.urjc.es>
2018-12-23 15:51:00 +0100

    Empezando proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -1,11 +1,45 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
-try:
+from uaclient import log
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
-    CONFIG = sys.argv[1]
 
-except IndexError:
-    print("Usage: python proxy_registrar.py config")
+class proxy(ContentHandler):
+	def __init__(self):
+		self.dic = {
+			"server": ["name","ip","puerto"],
+			"database": ["path", "passwdpath"],
+			"log":["path"],
+		}
+		self.config = {}
+	def start(self,name,attrs):
+		if name in self.dic:
+			for atribute in self.dic[name]:
+				self.conf[name+"_"+atribute] = attrs.get(atribute,"")
+	def get_tags(self):
+		return self.config
 
-print("Server MiServidor listening at port 5555")
+	
+	
+if __name__ == "__main__":
+	"""
+	Programa principal
+	"""
+	CONFIG = sys.argv[1]
+
+
+	try:
+	    print("Server MiServidor listening at port 5555...")
+	except IndexError:
+    	    sys.exit("Usage: python proxy_registrar.py config")
+
+# Parse del fichero XML.
+	parser = make_parser()
+	chandler = proxy
+	parser.setContentHandler(chandler)
+	parser.parse(open(CONFIG))
+	lista = ucharler.get_tags()
+	print(lista)
+
