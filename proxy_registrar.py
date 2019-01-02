127beb1ad66a38b074604c23955a4b97829561b7
Yolanda Lillo Mata <ylillo@alpha.aulas.gsyc.urjc.es>
2019-01-02 11:53:41 +0100

    Avanzando en el proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -7,6 +7,7 @@ from xml.sax.handler import ContentHandler
 import socket
 import socketserver
 import json
+from uaclient import log
 
 
 class proxy(ContentHandler):
@@ -28,21 +29,35 @@ class proxy(ContentHandler):
         return self.config
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+    """ Clase para un servidor SIP. """
 
     dic_usuarios= {}
-
-    def register2json(self):
-        json.dump(self.dic_usuarios, open(CONFIG["database_path"],"w"),indent=4)
-	 
 
     def json2registered(self): 
 # Abrir fichero y obtenemos diccionario.
         try:
-            with open(CONFIG["database_passwdpath"],"r") as file_json:
+            with open(DATABASE,"r") as file_json:
                 self.dic_usuarios = json.load(file_json)
-                print(self.dic_usuarios)
        	except:
             self.dic_usuarios = {}
+
+    def register2json(self):
+    # Introducimos usuario en el fichero JSON.
+        with open(DATABASE,"w") as file_json:
+            json.dump(self.dic_usuarios, file_json, sort_keys=True, indent=4)
+
+    def expired(self):
+    # Para borrar los usuarios registrados.
+        del_list = []
+        actual = time.strftime( '%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        for usuarios in self.dic_usuarios:
+            if self.dic_usuarios[usuarios]["expires"] <= actual:
+                del_list.append(usuarios)
+        for usuarios in del_list:
+            del self.dic_usuarios[usuarios]
+
+
+
 
 
 if __name__ == "__main__":
@@ -52,19 +67,33 @@ if __name__ == "__main__":
 
     try:
         CONFIG = sys.argv[1]
+        # Parse del fichero XML.
+        parser = make_parser()
+        cHandler = proxy()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(CONFIG))
+        lista = cHandler.get_tags()
+        print(lista)
+    
+        NAME = lista["server_name"]
+        SERVER = lista["server_ip"], int(lista["server_puerto"])
+        FILELOG = lista["log_path"]
+        DATABASE = lista["database_path"]
+        PASSWD_PATH = lista["database_passwdpath"]
+        PROXY_HEADER = "Via: SIP/2.0/UDP {}:{}".format(SERVER[0], SERVER[1])
+
     except IndexError:
     	sys.exit("Usage: python proxy_registrar.py config")
 
-# Parse del fichero XML.
-    parser = make_parser()
-    cHandler = proxy()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(CONFIG))
-    lista = cHandler.get_tags()
-    print(lista)
-    
-    NAME = lista["server_name"]
+    SERV = socketserver.UDPServer(SERVER, SIPRegisterHandler)
     print("Server " + NAME + " listening at port 5555...")
+    #FILELOG( "Server " + NAME + " listening at port 5555...")
+    try:
+        SERV.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit("\r\nClosed")
+
+
 
 
 
